dab941312420b5bbe1099dc20efc905e6cae4946
kallom <k.madjunov@gmail.com>
2018-03-26 12:36:43 +0300

    Add GET request for logout page

--- src/main/java/hotelapp/controllers/UserController.java
+++ src/main/java/hotelapp/controllers/UserController.java
@@ -69,4 +69,15 @@ public class UserController {
 
         return "base-layout";
     }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+
+        return "redirect:/login?logout";
+    }
 }

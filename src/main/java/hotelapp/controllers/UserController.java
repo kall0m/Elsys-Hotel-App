2809707ec4a952bc7235418c5d92856a17924a0a
kallom <k.madjunov@gmail.com>
2018-03-26 12:30:07 +0300

    Add GET request for register page

--- src/main/java/hotelapp/controllers/UserController.java
+++ src/main/java/hotelapp/controllers/UserController.java
@@ -1,4 +1,41 @@
 package hotelapp.controllers;
 
+import hotelapp.models.Role;
+import hotelapp.models.User;
+import hotelapp.services.RoleService;
+import hotelapp.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+    @Autowired
+    RoleService roleService;
+
+    @Autowired
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/register")
+    public String register(Model model) {
+        model.addAttribute("view", "user/register");
+
+        return "base-layout";
+    }
 }

8152f86246a81595073389e18fd8762335425a83
kallom <k.madjunov@gmail.com>
2018-03-27 16:18:08 +0300

    Add Boss Repository

--- src/main/java/hotelapp/repositories/BossRepository.java
+++ src/main/java/hotelapp/repositories/BossRepository.java
@@ -1,7 +1,6 @@
 package hotelapp.repositories;
 
 import hotelapp.models.Boss;
-import hotelapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BossRepository extends JpaRepository<Boss, Integer> {

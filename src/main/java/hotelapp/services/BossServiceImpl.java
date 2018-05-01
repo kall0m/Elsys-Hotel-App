9f40f822dcda242707f31726cadec1ebe80cf1b4
Sasho32 <sashosasho32@gmail.com>
2018-05-01 14:41:31 +0300

    Add bussiness logic

--- src/main/java/hotelapp/services/BossServiceImpl.java
+++ src/main/java/hotelapp/services/BossServiceImpl.java
@@ -1,5 +1,6 @@
 package hotelapp.services;
 
+import hotelapp.models.Board;
 import hotelapp.models.Boss;
 import hotelapp.repositories.BossRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,26 @@ public class BossServiceImpl implements BossService {
 
     public List<Boss> getAllBosses() {
         return bossRepository.findAll();
+    }
+
+    public boolean checkBoardsCount(Boss boss) {
+        return boss.getBoards().size() < 10;
+    }
+
+    public boolean checkTasksInBoardCount(Board board) {
+        return board.getTasks().size() < 10;
+    }
+
+    public boolean checkWorkerAccountsCount(Boss boss) {
+        if(boss.getSubscription() == 0) {
+            return boss.getWorkerAccounts() < 3;
+        } else if(boss.getSubscription() == 10) {
+            return boss.getWorkerAccounts() < 5;
+        } else if(boss.getSubscription() == 20) {
+            return boss.getWorkerAccounts() < 10;
+        }
+
+        return false;
     }
 
     public void saveBoss(Boss boss) {

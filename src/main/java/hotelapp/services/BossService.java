9f47b022c75ea540f63325f9ec9dc4515fdded0d
kallom <k.madjunov@gmail.com>
2018-05-01 14:41:31 +0300

    Add bussiness logic

--- src/main/java/hotelapp/services/BossService.java
+++ src/main/java/hotelapp/services/BossService.java
@@ -1,5 +1,6 @@
 package hotelapp.services;
 
+import hotelapp.models.Board;
 import hotelapp.models.Boss;
 
 import java.util.List;
@@ -8,6 +9,12 @@ public interface BossService {
     Boss findByEmail(String email);
 
     List<Boss> getAllBosses();
+
+    boolean checkBoardsCount(Boss boss);
+
+    boolean checkTasksInBoardCount(Board board);
+
+    boolean checkWorkerAccountsCount(Boss boss);
 
     void saveBoss(Boss boss);
 }

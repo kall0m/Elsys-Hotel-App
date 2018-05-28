0b2a4d27c0a897f906eb27645fd67ccb06974a20
kall0m <k.madjunov@gmail.com>
2018-05-28 05:10:08 +0300

    Try to get the PORT in the application run

--- src/main/java/hotelapp/HotelApplication.java
+++ src/main/java/hotelapp/HotelApplication.java
@@ -2,6 +2,7 @@ package hotelapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,6 +13,17 @@ public class HotelApplication {
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer() {
+		return (container -> {
+			container.setContextPath("");
+			if (System.getenv("PORT") != null) {
+				container.setPort(Integer.valueOf(System.getenv("PORT")));
+
+			}
+		});
 	}
 
 	public static void main(String[] args) {

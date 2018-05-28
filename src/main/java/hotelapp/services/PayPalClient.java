f50d27b888b82cd34091bb4c7adc829b4da0ed10
Sasho32 <sashosasho32@gmail.com>
2018-05-29 01:18:01 +0300

    Add PayPalClient Service

---
+++ src/main/java/hotelapp/services/PayPalClient.java
@@ -0,0 +1,59 @@
+package hotelapp.services;
+
+import com.paypal.api.payments.*;
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.PayPalRESTException;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PayPalClient {
+    String clientId = "ARgYDj9RIViPklbz5XbLe7sSZpaBboL5LufzxGekuEhs5nHJXe6bpQtT8fVFgGM6HeIrpZu966g2tWDZ";
+    String clientSecret = "EKmgk0VQtAjh4CeVCAThT-M8tu9HypB3-sgJM05CS2yqyRCdimOv1OdF7Q4r8M9oxdqEqnl1c3puO_HO";
+    
+    public Map<String, Object> createPayment(String sum){
+        Map<String, Object> response = new HashMap<String, Object>();
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(sum);
+        Transaction transaction = new Transaction();
+        transaction.setAmount(amount);
+        List<Transaction> transactions = new ArrayList<Transaction>();
+        transactions.add(transaction);
+
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(payer);
+        payment.setTransactions(transactions);
+
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl("http://localhost:4200/cancel");
+        redirectUrls.setReturnUrl("http://localhost:4200/");
+        payment.setRedirectUrls(redirectUrls);
+        Payment createdPayment;
+        try {
+            String redirectUrl = "";
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            createdPayment = payment.create(context);
+            if(createdPayment!=null){
+                List<Links> links = createdPayment.getLinks();
+                for (Links link:links) {
+                    if(link.getRel().equals("approval_url")){
+                        redirectUrl = link.getHref();
+                        break;
+                    }
+                }
+                response.put("status", "success");
+                response.put("redirect_url", redirectUrl);
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println("Error happened during payment creation!");
+        }
+        return response;
+    }
+}

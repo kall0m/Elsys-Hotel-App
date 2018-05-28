12a3b46fbb44c59589d5a1aab3864ed0a243719a
Sasho32 <sashosasho32@gmail.com>
2018-05-29 01:22:31 +0300

    Add complete payment method

--- src/main/java/hotelapp/controllers/PayPalController.java
+++ src/main/java/hotelapp/controllers/PayPalController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @RestController
@@ -23,5 +24,10 @@ public class PayPalController {
     @PostMapping(value = "/make/payment")
     public Map<String, Object> makePayment(@RequestParam("sum") String sum){
         return payPalClient.createPayment(sum);
+    }
+
+    @PostMapping(value = "/complete/payment")
+    public Map<String, Object> completePayment(HttpServletRequest request){
+        return payPalClient.completePayment(request);
     }
 }
--- src/main/java/hotelapp/services/PayPalClient.java
+++ src/main/java/hotelapp/services/PayPalClient.java
@@ -4,6 +4,7 @@ import com.paypal.api.payments.*;
 import com.paypal.base.rest.APIContext;
 import com.paypal.base.rest.PayPalRESTException;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -53,6 +54,26 @@ public class PayPalClient {
             }
         } catch (PayPalRESTException e) {
             System.out.println("Error happened during payment creation!");
+        }
+        return response;
+    }
+
+    public Map<String, Object> completePayment(HttpServletRequest req){
+        Map<String, Object> response = new HashMap();
+        Payment payment = new Payment();
+        payment.setId(req.getParameter("paymentId"));
+
+        PaymentExecution paymentExecution = new PaymentExecution();
+        paymentExecution.setPayerId(req.getParameter("PayerID"));
+        try {
+            APIContext context = new APIContext(clientId, clientSecret, "sandbox");
+            Payment createdPayment = payment.execute(context, paymentExecution);
+            if(createdPayment!=null){
+                response.put("status", "success");
+                response.put("payment", createdPayment);
+            }
+        } catch (PayPalRESTException e) {
+            System.err.println(e.getDetails());
         }
         return response;
     }

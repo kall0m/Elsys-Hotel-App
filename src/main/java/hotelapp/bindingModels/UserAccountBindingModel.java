6cce5cd8bc9b35dee9d9842ccdb4307a5a1ac0f9
Sasho32 <sashosasho32@gmail.com>
2018-05-30 09:07:48 +0300

    Add some pages and links to header

---
+++ src/main/java/hotelapp/bindingModels/UserAccountBindingModel.java
@@ -0,0 +1,60 @@
+package hotelapp.bindingModels;
+
+import javax.validation.constraints.NotNull;
+
+public class UserAccountBindingModel {
+    @NotNull
+    private String hotelName;
+
+    @NotNull
+    private String oldPassword;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String confirmPassword;
+
+    @NotNull
+    private String email;
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
